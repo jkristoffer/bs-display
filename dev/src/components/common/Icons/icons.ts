01b8bf2f2dfc1abbdedef34ac05ff868935ba575
Kris <one.kristoffer@gmail.com>
2025-05-13 17:01:26 +0800

    feat: create smart whiteboard buying guide page with components

---
+++ dev/src/components/common/Icons/icons.ts
@@ -0,0 +1,11 @@
+import CollaborationIcon from './collaboration-icon.png' assert { type: 'image' };
+import NetworkIcon from './network-icon.png' assert { type: 'image' };
+import LearningIcon from './learning-icon.png' assert { type: 'image' };
+import VersatileIcon from './versatile-icon.png' assert { type: 'image' };
+
+export const icons = {
+  collaboration: CollaborationIcon,
+  network: NetworkIcon,
+  learning: LearningIcon,
+  versatile: VersatileIcon
+};
